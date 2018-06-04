dee283563c548f80ba30d0bab39343e1a5639190
Shun Yao <ysmiracle@hotmail.com>
2018-06-04 03:17:27 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -8,6 +8,7 @@ export default class Product extends React.Component {
       <div className="product">
         <p>name: {this.props.name}</p>
         <p>producer: {this.props.producer}</p>
+<<<<<<< HEAD
         <p>hasWatermark: {this.props.hasWatermark}</p>
         <p>color: {this.props.color}</p>
         <p>weight: {this.props.weight}</p>
@@ -19,15 +20,37 @@ export default class Product extends React.Component {
 Product.defaultProps = {
   hasWatermark: false
 };
+=======
+        <p>hasWatermark: {this.props.hasWatermark}</p>//optional, defaults to false
+        <p>color: {this.props.color}</p>//a string — required, can only be 'white', 'eggshell-white' or 'salmon'
+        <p>weight: {this.props.weight}</p>//a number — required, ranges between 80 and 300
+      </div>
+    )
+  }
+}
+
+Product.defaultProps = {
+  hasWatermark: false;
+}
+>>>>>>> d7b09bba8c2ecbf080b8096c33035f983cbb5fcf
 
 Product.porpTypes = {
   name: PropTypes.string.isRequired,
   producer: PropTypes.string,
   hasWatermark: PropTypes.bool,
   color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
+<<<<<<< HEAD
   weight: function(props, propName, componentName) {
     if (props[propName] < 80 || props[propName] > 300) {
       return new Error('Invalid');
     }
   }
 };
+=======
+  weight: function(weight) {
+    if (weight < 80 || weight > 300) {
+      return new error('Invalid prop');
+    }
+  }
+}
+>>>>>>> d7b09bba8c2ecbf080b8096c33035f983cbb5fcf
